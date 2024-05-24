3275cb8925f75cfcda2be2fc13e7801cb291662e
Carson M. <carson@pyke.io>
2024-05-24 10:29:42 -0500

    upload artifacts

--- src/build.ts
+++ src/build.ts
@@ -1,10 +1,13 @@
 import { exists } from 'https://deno.land/std@0.224.0/fs/mod.ts';
 import { join } from 'https://deno.land/std@0.224.0/path/mod.ts';
 
-import { arch, cpus, platform } from 'node:os'; // is there really no deno-native function for this?
+import { arch as getArch, cpus, platform as getPlatform } from 'node:os';
 
 import { Command, EnumType } from '@cliffy/command';
 import $ from '@david/dax';
+
+const arch: 'x64' | 'arm64' = getArch();
+const platform: 'win32' | 'darwin' | 'linux' = getPlatform();
 
 const TARGET_ARCHITECTURE_TYPE = new EnumType([ 'x86_64', 'aarch64' ]);
 
@@ -42,7 +45,7 @@ await new Command()
 				args.push('-Donnxruntime_USE_TENSORRT_BUILTIN_PARSER=ON');
 			}
 
-			switch (platform()) {
+			switch (platform) {
 				case 'linux': {
 					const cudnnArchiveStream = await fetch(Deno.env.get('CUDNN_URL')!).then(c => c.body!);
 					const cudnnOutPath = join(root, 'cudnn');
@@ -81,26 +84,26 @@ await new Command()
 			}
 		}
 
-		if (platform() === 'win32' && options.directml) {
+		if (platform === 'win32' && options.directml) {
 			args.push('-Donnxruntime_USE_DIRECTML=ON');
 		}
-		if (platform() === 'darwin' && options.coreml) {
+		if (platform === 'darwin' && options.coreml) {
 			args.push('-Donnxruntime_USE_COREML=ON');
 		}
 		if (options.xnnpack) {
 			args.push('-Donnxruntime_USE_XNNPACK=ON');
 		}
 
-		if (platform() === 'darwin') {
+		if (platform === 'darwin') {
 			if (options.arch === 'aarch64') {
 				args.push('-DCMAKE_OSX_ARCHITECTURES=arm64');
 			} else {
 				args.push('-DCMAKE_OSX_ARCHITECTURES=x86_64');
 			}
 		} else {
-			if (options.arch === 'aarch64' && arch() !== 'arm64') {
+			if (options.arch === 'aarch64' && arch !== 'arm64') {
 				args.push('-Donnxruntime_CROSS_COMPILING=ON');
-				switch (platform()) {
+				switch (platform) {
 					case 'win32':
 						args.push('-A', 'ARM64');
 						args.push('-DCMAKE_CXX_FLAGS=-D_SILENCE_ALL_CXX23_DEPRECATION_WARNINGS');
@@ -118,18 +121,60 @@ await new Command()
 			args.push('-Donnxruntime_DISABLE_RTTI=OFF');
 		}
 
-		if (platform() === 'win32' && !options.static) {
+		if (platform === 'win32' && !options.static) {
 			args.push('-DONNX_USE_MSVC_STATIC_RUNTIME=OFF');
 			args.push('-Dprotobuf_MSVC_STATIC_RUNTIME=OFF');
 			args.push('-Dgtest_force_shared_crt=OFF');
 		}
 
+		if (!options.static) {
+			// actually, with CUDA & TensorRT, we could statically link the onnxruntime core (just not the EPs)
+			// ... could be the move (just needs the below fix for windows)
+			// 	if (platform === 'win32') {
+			// 		args.push('-DONNX_USE_MSVC_STATIC_RUNTIME=OFF');
+			// 		args.push('-Dprotobuf_MSVC_STATIC_RUNTIME=OFF');
+			// 		args.push('-Dgtest_force_shared_crt=OFF');
+			// 	}
+			args.push('-Donnxruntime_BUILD_SHARED_LIB=ON');
+		}
+
 		args.push('-Donnxruntime_BUILD_UNIT_TESTS=OFF');
 
 		const sourceDir = options.static ? join(root, 'src', 'static-build') : 'cmake';
+		const outDir = join(root, 'output');
 
-		await $`cmake -S ${sourceDir} -B build -D CMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_INSTALL_PREFIX=${join(root, 'output')} -DONNXRUNTIME_SOURCE_DIR=${onnxruntimeRoot} --compile-no-warning-as-error ${args}`;
+		await $`cmake -S ${sourceDir} -B build -D CMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_INSTALL_PREFIX=${outDir} -DONNXRUNTIME_SOURCE_DIR=${onnxruntimeRoot} --compile-no-warning-as-error ${args}`;
 		await $`cmake --build build --config Release --parallel ${cpus().length}`;
 		await $`cmake --install build --config Release`;
+
+		const artifactOutDir = join(root, 'artifact');
+		await Deno.mkdir(artifactOutDir);
+
+		const artifactLibDir = join(artifactOutDir, 'onnxruntime', 'lib');
+		await Deno.mkdir(artifactLibDir, { recursive: true });
+		const srcLibsDir = join(outDir, 'lib');
+
+		const staticLibName = (name: string) =>
+			`${platform !== 'win32' ? 'lib' : ''}${name}${platform !== 'win32' ? '.a' : '.lib'}`;
+		const dynamicLibName = (name: string) =>
+			`${platform !== 'win32' ? 'lib' : ''}${name}${platform === 'win32' ? '.dll' : platform === 'darwin' ? '.dylib' : '.so'}`;
+		const copyLib = async (filename: string) =>
+			await Deno.copyFile(join(srcLibsDir, filename), join(artifactLibDir, filename));
+
+		if (options.static) {
+			await copyLib(staticLibName('onnxruntime'));
+		} else {
+			await copyLib(dynamicLibName('onnxruntime'));
+			if (options.cuda || options.trt) {
+				await copyLib(dynamicLibName('onnxruntime_providers_shared'));
+			}
+
+			if (options.cuda) {
+				await copyLib(dynamicLibName('onnxruntime_providers_cuda'));
+			}
+			if (options.trt) {
+				await copyLib(dynamicLibName('onnxruntime_providers_tensorrt'));
+			}
+		}
 	})
 	.parse(Deno.args);
