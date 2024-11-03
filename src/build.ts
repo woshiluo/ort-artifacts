70851fddec822b51a3201cf2c31882987c0bf3d6
Carson M. <carson@pyke.io>
2024-11-03 15:15:35 -0600

    Ok

--- src/build.ts
+++ src/build.ts
@@ -32,7 +32,7 @@ await new Command()
 
 		const onnxruntimeRoot = join(root, 'onnxruntime');
 		if (!await exists(onnxruntimeRoot)) {
-			await $`git clone https://github.com/microsoft/onnxruntime --recursive --single-branch --depth 1 --branch rel-1.19.0`;
+			await $`git clone https://github.com/microsoft/onnxruntime --recursive --single-branch --depth 1 --branch rel-${options.upstreamVersion}`;
 		}
 
 		$.cd(onnxruntimeRoot);
